90c9e85a2951336cfb11cf0805fdb792de3305c2
Angel <jeal.code47@gmail.com>
2025-03-03 11:40:48 -0300

    fix: build

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -1,13 +1,20 @@
-import { Suspense } from "react";
+"use client";
+import { Suspense, use } from "react";
 import TasksList from "@/app/projects/(ui)/TaskList";
 import { fetchTasksUseCase } from "@/app/infra/container";
+import { useParams } from "next/navigation";
 
-export default async function ProjectPage({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const tasks = await fetchTasksUseCase.execute(params.id);
+export default function ProjectPage() {
+  const params = useParams();
+  if (params === undefined || params.id === undefined) {
+    return <p>Project not informed</p>;
+  }
+  const tasksList = async () =>
+    await fetchTasksUseCase.execute(
+      Array.isArray(params.id) ? params.id[0] : params.id || ""
+    );
+
+  const tasks = use(tasksList());
 
   return (
     <div>
